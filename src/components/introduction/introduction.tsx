9c30ffece03978fafe60df45161c32786ca2d4f9
Eduardo <edu.manoel.p@gmail.com>
2019-11-15 13:51:35 -0300

    First page refactor

--- src/components/introduction/introduction.tsx
+++ src/components/introduction/introduction.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import "../../App.css";
 import "./introduction.css";
-import { Typography } from '@material-ui/core';
+import { Typography, Hidden } from '@material-ui/core';
 import { ArrowDownward } from '@material-ui/icons';
 
 class Introduction extends React.Component {
@@ -9,18 +9,38 @@ class Introduction extends React.Component {
         return (
             <div className={"App-introduction"}>
                 <div className={"hilight"}></div>
-                <div className={"intro-text-container"}>
-                    <Typography color="primary">
-                        <h2>Bem-vinda(o),</h2>
-                        <span>
-                        a Menezes &amp; Moreira Assessoria Contábil é uma empresa que tem como atividade
-                        principal a prestação de serviços na área contábil, envolvendo terceirização operacional,
-                        assessoria, contabilidade consultiva e empresarial, voltados para empresas de diversos
-                        ramos de atividade.
-                        </span>
-                        <ArrowDownward className={"more-icon"}/>
-                    </Typography>
-                </div>
+                <Hidden smDown>
+                    <div className={"intro-logo"}>
+                        <img src={process.env.PUBLIC_URL + '/images/card.svg'} alt={'Contabilidade Menezes e Moreira'}/>
+                        <Typography variant="h2" color="primary">Contabilidade Menezes e Moreira</Typography>
+                    </div>
+                    <div className={"intro-text-container"}>
+                        <Typography color="primary">
+                            <h2>Bem-vinda(o),</h2>
+                            <Hidden smDown>
+                                <span>
+                                a Menezes &amp; Moreira Assessoria Contábil é uma empresa que tem como atividade
+                                principal a prestação de serviços na área contábil, envolvendo terceirização operacional,
+                                assessoria, contabilidade consultiva e empresarial, voltados para empresas de diversos
+                                ramos de atividade.
+                                </span>
+                            </Hidden>
+                            <ArrowDownward className={"more-icon"}/>
+                        </Typography>
+                    </div>
+                </Hidden>
+                <Hidden mdUp>
+                    <div className={"intro-logo-smDown"}>
+                        <img src={process.env.PUBLIC_URL + '/images/card.svg'} alt={'Contabilidade Menezes e Moreira'}/>
+                        <Typography variant="h4" color="primary">Contabilidade Menezes e Moreira</Typography>
+                    </div>
+                    <div className={"intro-text-container"}>
+                        <Typography color="primary">
+                            <h2>Bem-vinda(o)!</h2>
+                            <ArrowDownward className={"more-icon"}/>
+                        </Typography>
+                    </div>
+                </Hidden>
             </div>
         );
     }
